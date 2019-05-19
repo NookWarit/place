d72728eb095b277f9d75582936da64fb68494c8a
warittha <nookzzy@tuktukpass.com>
2019-05-19 22:47:40 +0700

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by warittha on 19/5/2562 BE.
+//  Copyright © 2562 warittha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
